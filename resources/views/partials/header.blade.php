1a3016c4a235e7210ccf02afa75f8831f8687f05
Susana Piñero <susananzth@gmail.com>
2020-07-04 15:14:19 -0500

    Añadiendo mis estilos

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+            <div class="content">
+              <div class="flex-container">
+                  <div class="bg-header col-s-12 col-m-6"></div>
+                  <div class="col-s-12 col-m-6 title-header">
+                      <div class="pt-1">
+                          <h1>My Guide Laravel 6</h1>
+                          <h3>by Susana Piñero</h3>
+                          <h3>@susananzth</h3>
+                      </div>
+                  </div>
+              </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,13 +1,22 @@
-@if (Route::has('login'))
-    <div class="top-right links">
-        @auth
-            <a href="{{ url('/home') }}">Home</a>
-        @else
-            <a href="{{ route('login') }}">Login</a>
+<nav class="text-center">
+  @if (Route::has('login'))
+      <div class="top-right links">
+          @auth
+              <a href="{{ url('/home') }}">Panel administrativo</a>
+          @else
+              <a href="{{ route('login') }}">Iniciar sesión</a>
 
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}">Register</a>
-            @endif
-        @endauth
-    </div>
-@endif
+              @if (Route::has('register'))
+                  <a href="{{ route('register') }}">Registrarse</a>
+              @endif
+          @endauth
+      </div>
+
+  @endif
+    <ul class="flex-container col-center li-noStyle text-center">
+        <li class="col-nav padd2"><a href="#">Título1</a></li>
+        <li class="col-nav padd2"><a href="#">Título2</a></li>
+        <li class="col-nav padd2"><a href="#">Título3</a></li>
+        <li class="col-nav padd2"><a href="#">Título4</a></li>
+    </ul>
+</nav>
