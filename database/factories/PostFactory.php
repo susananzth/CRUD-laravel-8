9b80e6ec44888d3c75313510708bf345921b7241
Susana Piñero <susananzth@gmail.com>
2021-04-03 03:49:53 -0500

    Creando archivos para Post. Creando  factory de los post

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        /**
+         * Para generar los registros, abrir la consola de tinker con
+         * > php artisan tinker
+         * Luego ingresar el comando:
+         * > Post::factory()->count(10)->create()
+         */
+        return [
+            'title'=>$this->faker->sentence
+        ];
+    }
+}
